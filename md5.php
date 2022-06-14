85d30cd874d348036656f5bcd63752508ea81159
Paramin Supapong <74184959+paramin008@users.noreply.github.com>
2022-06-14 21:28:40 +0700

    Update and rename md5 random.md to md5.php

--- md5.php
+++ md5.php
@@ -1,9 +1,11 @@
 <?php
+ 
 function random_strings($length_of_string) {
+     
     return substr(md5(time()), 0, $length_of_string);
 }
-echo random_strings(10);
  
-echo "\n"
+echo random_strings(1000);
  
+echo "\n";
 ?>
